6d18c17893a58e8ae45627650bf666ddeebffacf
Verigg <116000346+Verigg@users.noreply.github.com>
2025-01-18 05:52:28 +1000

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,44 +1,25 @@
 @echo off
 setlocal
 
+:: Путь к корневой директории проекта
 set REPO_DIR=%~dp0
 
+:: Перейти в папку с репозиторием
 cd /d %REPO_DIR%
 
-echo Updating repository...
-git pull @echo off
-setlocal
-
-set REPO_DIR=%~dp0
-
-cd /d %REPO_DIR%
-
+:: Обновить репозиторий с помощью Git
 echo Updating repository...
 git pull
 
+:: Для каждой папки с лабораторной работой, например, Lab1, Lab2, Lab3...
 for /d %%F in (Lab*) do (
     echo Building project in folder %%F...
     cd %%F
     mkdir build 2>nul
     cd build
-    cmake ..
+    cmake -G "MinGW Makefiles" ..
     cmake --build .
     cd ..
-)
-
-echo All projects have been built successfully!
-
-endlocal
-pause
-
-
-for /d %%F in (Lab*) do (
-    echo Building project in folder %%F...
-    cd %%F
-    mkdir build 2>nul
-    cd build
-    cmake ..
-    cmake --build .
     cd ..
 )
 
