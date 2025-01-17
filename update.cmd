40870bb2465d556c4577a00ae9b888bebe937892
Verigg <116000346+Verigg@users.noreply.github.com>
2025-01-18 09:57:54 +1000

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,17 +1,13 @@
 @echo off
 setlocal
 
-:: Путь к корневой директории проекта
 set REPO_DIR=%~dp0
 
-:: Перейти в папку с репозиторием
 cd /d %REPO_DIR%
 
-:: Обновить репозиторий с помощью Git
 echo Updating repository...
 git pull
 
-:: Для каждой папки с лабораторной работой, например, Lab1, Lab2, Lab3...
 for /d %%F in (Lab*) do (
     echo Building project in folder %%F...
     cd %%F
